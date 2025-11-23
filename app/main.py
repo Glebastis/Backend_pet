3efb24a8c5d70d1288aca2c0060d5da6a0db5945
Glebastis <babenkogleb@gmail.com>
2025-11-23 18:48:00 +0300

    Простейшая html страничка для наглядного тестирования API

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 import uvicorn
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotel.router import router as router_hotel
 
 from app.pages.router import router as router_pages
-
+from app.images.router import router as router_images
 
 app = FastAPI()
 
@@ -16,3 +18,15 @@ app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotel)
 app.include_router(router_pages)
+app.include_router(router_images)
+app.include_router(router_images)
+
+origins = ['http://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Authorization"],
+)
